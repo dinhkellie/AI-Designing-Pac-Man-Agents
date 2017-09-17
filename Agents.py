04a606ccda080cb40c0054e635ee7b950b67e501
Kellie Dinh <dinhkellie@yahoo.com>
2017-09-17 14:16:45 -0400

    Add files via upload

--- Agents.py
+++ Agents.py
@@ -2,24 +2,27 @@
 
 from game import Agent
 from game import Directions
+import random
 class DumbAgent(Agent):
-    "An agent that goes West until it can't."
+    #"An agent that goes West until it can't."
     def getAction(self, state):
-        "The agent receives a GameState (defined in pacman.py)."
+        #"The agent receives a GameState (defined in pacman.py)."
         print "Location: ", state.getPacmanPosition()
         print "Actions available: ", state.getLegalPacmanActions()
         if Directions.WEST in state.getLegalPacmanActions():
             print "Going West."
             return Directions.WEST
-        elif Directions.EAST in state.getLegalPacmanActions():
-            print "Going East."
-            return Directions.EAST
-        elif Directions.SOUTH in state.getLegalPacmanActions():
-            print "Going South."
-            return Directions.South
-        elif Directions.NORTH in state.getLegalPacmanActions():
-            print "Going NORTH."
-            return Directions.NORTH
-        else:
-            print "Going West I guess."
-            return Directions.WEST
+
+class RandomAgent(Agent):
+    def getAction(self, state):
+        print "Location: ", state.getPacmanPosition()
+        possible = state.getLegalPacmanActions()
+        possible.remove(Directions.STOP)
+        print "Actions available: ", possible
+        randomDir = random.choice(possible)
+        print "Going: ", randomDir
+        return randomDir
+
+class ReflexAgent(Agent):
+    def getAction(self, state):
+        
